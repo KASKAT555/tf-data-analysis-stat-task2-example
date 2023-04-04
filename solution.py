dd422980395ef1e4ffa3ed121536077ffe9e5edc
KASKAT555 <129871841+KASKAT555@users.noreply.github.com>
2023-04-04 19:43:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,12 +7,13 @@ from scipy.stats import norm
 chat_id = 1015647760 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    t_alpha = t.ppf(1 - (1-p)/2, n-2)
-    x_mean = np.mean(x)
-    s = np.sqrt(np.sum((x-x_mean)**2) / (n-1))
-    se = s / np.sqrt(n)
-    t_val = se * t_alpha
-    a = x_mean - t_val / 2
-    b = x_mean + t_val / 2
-    return (a, b)
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha = 1 - p
+    n = x.size
+    sum_x_2 = sum([e ** 2 for e in x])
+    q_l = chi2(2 * n).ppf(q=alpha / 2)
+    q_r = chi2(2 * n).ppf(q=1 - alpha / 2)
+    # print(q_l, q_r)
+    return np.sqrt(sum_x_2 / q_r / 56), np.sqrt(sum_x_2 / q_l / 56)
