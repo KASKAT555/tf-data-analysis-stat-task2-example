88114b2d5b83358d1121f6d0e8ae533e823e3382
KASKAT555 <129871841+KASKAT555@users.noreply.github.com>
2023-04-04 18:26:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,19 +7,12 @@ from scipy.stats import norm
 chat_id = 1015647760 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Calculate sample mean and standard deviation
     n = len(x)
-    sample_mean = np.mean(x)
-    sample_std = np.std(x, ddof=1)
-    
-    # Calculate t-value for two-tailed test with (1-p) confidence level and n-1 degrees of freedom
-    t_value = abs(norm.ppf((1-p)/2))
-    
-    # Calculate margin of error
-    margin_of_error = t_value * sample_std / np.sqrt(n)
-    
-    # Calculate confidence interval
-    lower_bound = sample_mean - margin_of_error
-    upper_bound = sample_mean + margin_of_error
-    
-    return (lower_bound, upper_bound)
+    t_alpha = t.ppf(1 - (1-p)/2, n-2)
+    x_mean = np.mean(x)
+    s = np.sqrt(np.sum((x-x_mean)**2) / (n-1))
+    se = s / np.sqrt(n)
+    t_val = se * t_alpha
+    a = x_mean - t_val / 2
+    b = x_mean + t_val / 2
+    return (a, b)
