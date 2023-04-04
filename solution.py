0a8a2e3695c08d18b8d45b15be7120cb5a76bac5
KASKAT555 <129871841+KASKAT555@users.noreply.github.com>
2023-04-04 18:11:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,18 +7,19 @@ from scipy.stats import norm
 chat_id = 1015647760 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Вычисляем среднее значение измерений
-    x_mean = np.mean(x)
+    # Calculate sample mean and standard deviation
+    n = len(x)
+    sample_mean = np.mean(x)
+    sample_std = np.std(x, ddof=1)
     
-    # Вычисляем стандартное отклонение измерений
-    x_std = np.std(x, ddof=1)
+    # Calculate t-value for two-tailed test with (1-p) confidence level and n-1 degrees of freedom
+    t_value = abs(norm.ppf((1-p)/2))
     
-    # Вычисляем критическое значение z_alpha/2
-    z_alpha_2 = norm.ppf(1 - (1 - p) / 2)
+    # Calculate margin of error
+    margin_of_error = t_value * sample_std / np.sqrt(n)
     
-    # Вычисляем доверительный интервал
-    interval_half_width = z_alpha_2 * x_std / np.sqrt(len(x))
-    interval_left = x_mean - interval_half_width
-    interval_right = x_mean + interval_half_width
+    # Calculate confidence interval
+    lower_bound = sample_mean - margin_of_error
+    upper_bound = sample_mean + margin_of_error
     
-    return (interval_left, interval_right)
+    return (lower_bound, upper_bound)
