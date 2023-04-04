54722597ae93605b3024a5a530fd9c23adadad76
KASKAT555 <129871841+KASKAT555@users.noreply.github.com>
2023-04-04 18:00:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,18 @@ from scipy.stats import norm
 chat_id = 1015647760 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    s = np.sqrt(np.mean(x**2) - np.mean(x)**2)
-    z = norm.ppf(1 - (1 - p) / 2)
-    delta = z * s / np.sqrt(n)
-    mean = np.mean(x)
-    left = mean - delta
-    right = mean + delta
-    return (left, right)
+    # Вычисляем среднее значение измерений
+    x_mean = np.mean(x)
+    
+    # Вычисляем стандартное отклонение измерений
+    x_std = np.std(x, ddof=1)
+    
+    # Вычисляем критическое значение z_alpha/2
+    z_alpha_2 = norm.ppf(1 - (1 - p) / 2)
+    
+    # Вычисляем доверительный интервал
+    interval_half_width = z_alpha_2 * x_std / np.sqrt(len(x))
+    interval_left = x_mean - interval_half_width
+    interval_right = x_mean + interval_half_width
+    
+    return (interval_left, interval_right)
