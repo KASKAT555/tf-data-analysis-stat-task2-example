d5e5b1da30f537c656a3c81b817fc8b1c8272e1e
KASKAT555 <129871841+KASKAT555@users.noreply.github.com>
2023-04-04 17:44:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,12 +8,10 @@ chat_id = 1015647760 # Ваш chat ID, не меняйте название пе
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
-    t_alpha = t.ppf((1+p)/2, n-1)
-    S = np.std(x, ddof=1)
-    mean_path_length = np.mean(x)
-    a = 2 * mean_path_length / (92 ** 2)
-    b = S * np.sqrt(n/2) / (mean_path_length ** 2)
-    delta = t_alpha * b
-    left_bound = a - delta
-    right_bound = a + delta
-    return left_bound, right_bound
+    s = np.sqrt(np.mean(x**2) - np.mean(x)**2)
+    z = norm.ppf(1 - (1 - p) / 2)
+    delta = z * s / np.sqrt(n)
+    mean = np.mean(x)
+    left = mean - delta
+    right = mean + delta
+    return (left, right)
