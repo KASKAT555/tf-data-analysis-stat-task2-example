d0a3758e03fa8ec7c37139eef1206e01b8317f3d
KASKAT555 <129871841+KASKAT555@users.noreply.github.com>
2023-04-04 17:16:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,13 @@ from scipy.stats import norm
 chat_id = 1015647760 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    t_alpha = t.ppf((1+p)/2, n-1)
+    S = np.std(x, ddof=1)
+    mean_path_length = np.mean(x)
+    a = 2 * mean_path_length / (92 ** 2)
+    b = S * np.sqrt(n/2) / (mean_path_length ** 2)
+    delta = t_alpha * b
+    left_bound = a - delta
+    right_bound = a + delta
+    return left_bound, right_bound
